b2bfbfefbe4251ffb19e2f37729dd92ead37bcec
Gümrah Sindar <99037179+gumrahsindar@users.noreply.github.com>
2023-05-10 17:03:29 +0300

    Complete introducing section

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ function App() {
     // global container
     <div className='container grid items-center just mx-auto min-h-screen'>
       {/* inner container */}
-      <div className='grid md:grid-cols-2 mx-8 md:mx-auto py-7 md:p-10 '>
+      <div className='grid max-w-5xl md:grid-cols-2 mx-8 md:mx-auto py-7 md:p-10 '>
         {/* header area */}
-        <div className='bg-white grid md:col-span-2 p-6 pt-7 pb-8 md:p-10 md:pb-10 rounded-t-md drop-shadow-xl'>
+        <div className='bg-white grid md:col-span-2 p-6 pt-7 pb-8 md:p-10 md:pb-10 rounded-t-md '>
           <h1 className='text-primary-cyan text-[20px] font-bold tracking-tight mb-6 '>
             Join our community
           </h1>
@@ -16,12 +16,12 @@ function App() {
           </h3>
           <h5 className='text-neutral-blue text-sm leading-7'>
             Gain access to our full library of tutorials along with expert code
-            reviews. Perfect for any developers who are serious about honing
-            their skills.
+            reviews. <br /> Perfect for any developers who are serious about
+            honing their skills.
           </h5>
         </div>
         {/* pricing area */}
-        <div className='p-6 md:p-10 bg-primary-cyan rounded-bl-md drop-shadow-xl '>
+        <div className='p-6 md:p-10 bg-primary-cyan md:rounded-bl-md '>
           <h2 className='text-lg font-bold text-white tracking-tight'>
             Monthly Subscription
           </h2>
@@ -35,6 +35,23 @@ function App() {
           <button className='py-3 px-8 w-full text-white font-bold bg-primary-yellow shadow-lg rounded-md hover:bg-opacity-90 duration-500  '>
             Sign Up
           </button>
+        </div>
+        {/* introducing area */}
+        <div className='p-6 md:p-10 bg-primary-cyan bg-opacity-80 rounded-b-md md:rounded-b-none md:rounded-br-md '>
+          <h2 className='text-lg font-bold text-white tracking-tight'>
+            Why Us
+          </h2>
+          <h3 className='text-white text-sm opacity-75 mt-[18px]'>
+            <ul className='leading-[21px]'>
+              <li>Tutorials by industry experts</li>
+              <li>Peer & expert code review</li>
+              <li>Coding exercises</li>
+              <li>Access to our GitHub repos</li>
+              <li>Community forum</li>
+              <li>Flashcard decks</li>
+              <li>New videos every week</li>
+            </ul>
+          </h3>
         </div>
       </div>
     </div>
